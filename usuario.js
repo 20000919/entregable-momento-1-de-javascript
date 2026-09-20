84c045ec5ea47d6a0e9a039a8abe65ec389b0315
unknown <dubantique5@gmail.com>
2026-09-20 12:38:06 -0500

    se agregaron mas variables y se ha iniciado con el while

--- usuario.js
+++ usuario.js
@@ -2,5 +2,17 @@ function validarAcceso() {
 
     const USUARIOCORRECTO = "admin";
     const CONTRASEÑACORRECTA = "6789"
+    const MAXIMOINTENTOS = 3;
+    let intentos = 0;
+    let accesoConcedido = false;
+
+    while (intentos < MAXIMOINTENTOS && !accesoConcedido) {
+        intentos++;
+
+
+
+        
+        
+    }
     
 }
